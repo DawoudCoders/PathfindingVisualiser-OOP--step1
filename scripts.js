ffe316c44ef6a8bb286f1711a030752684e9ac92
dawoudcoders <dawoudtanveer@outlook.com>
2022-05-01 12:24:29 +0100

    Further file structure setup, grid class and config setup

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-const grid = new Grid();
+import Grid from "./app/grid/Grid.class.js";
+import gridConfig from "./app/config/grid.config.js";
+const grid = new Grid(gridConfig);
+
+grid.build();
+grid.draw();
+
+console.log(grid.settings);
